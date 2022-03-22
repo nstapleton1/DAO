8a0c16edf98d6efc5b55cdc4e0fedba6397b56a8
Niall Stapleton <niall.stapleton@gmail.com>
2022-03-22 19:28:53 +0000

    inital commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xB054427c83f58Dd637aB2a2883FFaE316FBbFdB4";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
